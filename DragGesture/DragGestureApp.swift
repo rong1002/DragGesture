7524210fcdadbc0fb7239f49402a977d07c26218
rong1002 <borong1002@gmail.com>
2021-01-20 13:15:18 +0800

    Initial Commit

---
+++ DragGesture/DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureApp.swift
+//  DragGesture
+//
+//  Created by Lin Bo Rong on 2021/1/20.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
